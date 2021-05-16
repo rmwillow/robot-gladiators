2efc70a974bd9cfee2dba9d1bdd423704908c0f6
rmwillow <rmwillowbis2bis@gmail.com>
2021-05-16 14:38:03 -0500

    playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,6 +81,7 @@ var startGame = function() {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
+
             // pick new enemy to fight based on the index of the enemyInfo array
             var pickedEnemyObj = enemyInfo[i];
 
@@ -168,6 +169,18 @@ var shop = function() {
 /* END GAME FUNCTIONS */
 
 /* GAME INFORMATION / VARIABLES */
+
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    // ***************************************
+    // ADD LOOP HERE WITH PROMPT AND CONDITION
+    // ***************************************
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
 
 // player information
 var playerInfo = {
